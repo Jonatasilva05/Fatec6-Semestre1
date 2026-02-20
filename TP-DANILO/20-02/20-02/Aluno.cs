43a66b055594662d08310f400b5be21d0a90e8e0
Jonatasilva05 <jonatasmoraes05@gmail.com>
2026-02-20 11:24:08 -0300

    20-02

---
+++ TP-DANILO/20-02/20-02/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_02
+{
+    public class Aluno
+    {
+        public string? nome;
+        public string? genero;
+        public string? ra;
+        public int idade;
+    }
+}

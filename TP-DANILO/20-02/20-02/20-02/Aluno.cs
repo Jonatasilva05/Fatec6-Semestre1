fb80846981d880f06cdad237e58b5df1d039f1e4
Jonatasilva05 <jonatasmoraes05@gmail.com>
2026-02-20 12:02:35 -0300

    20-02

---
+++ TP-DANILO/20-02/20-02/20-02/Aluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_02
+{
+    public class Carro
+    {
+        //Caracteristicas do Carro = Propriedades/Atributos
+
+        public string? marca; // A ? (interrogação é porque ele pode ser umm valor nulo)
+        public string? modelo;
+        public int ano;
+        public string? placa;
+        public string? nome;
+        public string? idade;
+            
+        //Ações do Carro - Metodos/Comportamentos
+
+        public void MostrarInfoCarro()
+        {
+            ;
+            Console.WriteLine("Marca: " + marca);
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Ano: " + ano);
+            Console.WriteLine("Placa: " + placa);
+        }
+        public void acelerar() //void e para indicar que é um metodo
+        {
+            Console.WriteLine("O carro acelerou");
+        }
+
+        public void brecar()
+        {
+            Console.WriteLine("O carro freou");
+        }
+
+        public void ligarCarro()
+        {
+            Console.WriteLine("O carro Ligou");
+        }
+
+        public void desligarCarro()
+        {
+            Console.WriteLine("O carro Desligou");
+
+        }
+    }
+}

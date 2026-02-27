1e04ded3538f0745f7b40dfa2d9f7563060ea305
Jonatasilva05 <jonatasmoraes05@gmail.com>
2026-02-27 12:15:13 -0300

    27-02

---
+++ TP-DANILO/27-02/27-02/27-02/ContaBancaria.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_02
+{
+    internal class ContaBancaria
+    {
+        private double saldo;
+
+
+        //METODO PARA LIBERAR O ACESSO DO OBEJTO A NOSSA CLASSE PRIVADA
+        //ATRIBUIR VALOR
+        public void setSaldo(double _saldo)
+        {
+            saldo = _saldo;
+        }
+
+        //LER O VALOR
+        public double getSaldo()
+        {
+            return saldo;
+        }
+
+
+        private double limiteConta;
+
+        public void setLimiteConta(double _limiteConta)
+        {
+            limiteConta = _limiteConta;
+        }
+
+        public double getLimiteConta()
+        {
+            return limiteConta;
+        }
+
+
+        public string? titular;
+        public int numeroConta;
+        public string? perfil;
+
+
+        //CONSTRUTOR PADRÃO
+        public ContaBancaria()
+        {
+        }
+
+
+        //CONSTRUTOR 2
+        public ContaBancaria(double _saldo)
+        {
+            saldo = _saldo;
+        }
+
+
+        //CONSTRUTOR 3
+        public ContaBancaria(string _titular, int _numeroConta)
+        {
+            titular = _titular;
+            numeroConta = _numeroConta;
+        }
+
+        //CONTRUTOR 4 
+        public ContaBancaria(string _perfil)
+        { 
+            perfil = _perfil;
+        }
+    }
+}
---
+++ TP-DANILO/27-02/27-02/27-02/Program.cs
@@ -0,0 +1,28 @@
+﻿using _27_02;
+
+//CRIANDO O OBJETO ULTILIZANDO O CONSTRUTOR PADRÃO
+ContaBancaria contaBancaria1 = new ContaBancaria();
+contaBancaria1.setSaldo(800);
+Console.WriteLine("O valor da sua conta é: " + contaBancaria1.getSaldo());
+
+
+//CRIANDO O OBJETO ULTILIZANDO O CONTRUTOR 2
+ContaBancaria contaBancaria2 = new ContaBancaria(500);
+
+
+//CRIANDO O OBJETO ULTILIZANDO O CONTRUTOR 3
+ContaBancaria contaBancaria3 = new ContaBancaria("Maicon", 12589702);
+
+
+//CRIANDO O OBJETO ULTILIZANDO O CONTRUTOR 4
+ContaBancaria contaBancaria4 = new ContaBancaria(1000.50);
+
+
+//CRIANDO O OBJETO ULTILIZANDO O CONTRUTOR 5
+ContaBancaria contaBancaria5 = new ContaBancaria("basico");
+
+
+
+ContaBancaria contaBancaria6 = new ContaBancaria();
+contaBancaria6.setLimiteConta(1500);
+Console.WriteLine("O Limite da sua conta é: " + contaBancaria6.getLimiteConta());

7068e13cafc0ae5eddf5da1cbbe61dba1ce0f9c4
Jonatasilva05 <jonatasmoraes05@gmail.com>
2026-03-20 12:02:49 -0300

    20-03

---
+++ TP-DANILO/20-03/20-03/ConsoleApp1/CartaoDebito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_03
+{
+    public class CartaoDebito : Pagamento, IPagamento
+    {
+        public CartaoDebito(decimal _valorPagamentoDebito, string? _nome) 
+        {   
+            setValorPagamento(_valorPagamentoDebito);
+            nome = _nome;
+        }
+        public void processarPagamento()
+        {
+            Console.WriteLine($"Pagamento {nome} com cartão de debito, no valor de {getValorPagamento()} realizado com sucesso");
+        }
+        public void estornarPagamento()
+        {
+            Console.WriteLine($"Estorno {nome} cartão de debito, no valor de {getValorPagamento} realizado com sucesso");
+        }
+    }
+}
---
+++ TP-DANILO/20-03/20-03/ConsoleApp1/IPagamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_03
+{
+    public interface IPagamento
+    {
+        public void processarPagamento();
+
+        public void estornarPagamento();
+    }
+}

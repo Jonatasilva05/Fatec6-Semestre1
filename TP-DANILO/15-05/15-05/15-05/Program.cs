b703862174d77060628e478ca8ae1635dd0adbc1
Jonatasilva05 <jonatasmoraes05@gmail.com>
2026-05-15 11:46:41 -0300

    08-05

--- TP-DANILO/15-05/15-05/15-05/Program.cs
+++ TP-DANILO/15-05/15-05/15-05/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using _15_05.Models;
+using _15_05.Repository;
+
+AlunoRepository repo = new AlunoRepository();
+
+// CRIANDO UM OBJETO PARA ALUNO
+Aluno alunoNovo = new Aluno();
+
+Console.WriteLine("Digite o Nome do Aluno: ");
+alunoNovo.nome = Console.ReadLine();
+
+Console.WriteLine("Digite a Idade do Aluno: ");
+alunoNovo.idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o Email do Aluno: ");
+alunoNovo.email = Console.ReadLine();
+
+repo.inserir(alunoNovo);
---
+++ TP-DANILO/15-05/15-05/15-05/Repository/AlunoRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using _15_05.Models;
+
+namespace _15_05.Repository
+{
+    public class AlunoRepository
+    {
+        string conexao = "Server=192.168.0.39;Database=BD_FACULDADE_JONATAS;User Id=aluno;Password=Aluno@123;Encrypt=False;TrustServerCertificate=True;";
+
+        public void inserir(Aluno alunoNovo)
+        {
+            using (SqlConnection conn = new SqlConnection(conexao))
+            {
+                string query = "INSERT INTO ALUNO (NOME, IDADE, EMAIL) VALUES (@NOME, @IDADE, @EMAIL);";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@NOME", alunoNovo.nome);
+                cmd.Parameters.AddWithValue("@IDADE", alunoNovo.idade);
+                cmd.Parameters.AddWithValue("@EMAIL", alunoNovo.email);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Aluno Inserido com Sucesso");
+            }
+        }
+    }
+}
